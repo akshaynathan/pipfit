a17187dd0d8c308fa3ef55f1dcd63d18ae85650d
Akshay Nathan <akshay.nathan@yale.edu>
2015-04-28 00:19:40 -0400

    changed org for cljs

---
+++ src/pipfit/web/cljs/login.cljs
@@ -0,0 +1,46 @@
+(ns ^:figwheel-always pipfit.web.cljs.login
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [figwheel.client :as fw]
+            [om-bootstrap.input :as i]
+            [om-bootstrap.button :as b]
+            [om-tools.dom :as d :include-macros :true]
+            [cljs-http.client :as http]
+            [om-tools.core :refer-macros [defcomponentk]]
+            [cljs.core.async :refer [put! chan <!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(defn sign-in
+  "Sign the user in using ajax."
+  [data owner]
+  (let [username (.-value (om/get-node owner "ufield"))
+        password (.-value (om/get-node owner "pfield"))]
+    (go (let [response (<! (http/post "/login" {:json-params {:username username
+                                                              :password password}}))]
+          (prn (:status response))
+          (prn (:body response))))))
+
+(defn handle-change
+  "Modify state based on changes in username or password field."
+  [e owner k]
+  (om/set-state! owner k (.. e -target -value)))
+
+(defn login-form [data owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:username "" :password ""})
+    om/IRenderState
+    (render-state [this state]
+      (dom/div nil
+       (dom/h2 nil "Login")
+       (dom/div nil
+        (dom/input #js {:type "text"
+                        :ref "ufield"
+                        :onChange #(handle-change % owner :username)
+                        :value (:username state)})
+        (dom/input #js {:type "password"
+                        :ref "pfield"
+                        :onChange #(handle-change % owner :password)
+                        :value (:password state)}) 
+        (dom/button #js {:onClick #(sign-in data owner)} "Submit"))))))
--- src/pipfit/web/core.cljs
+++ src/pipfit/web/core.cljs
@@ -3,6 +3,7 @@
             [om.dom :as dom :include-macros true]
             [figwheel.client :as fw]
             [om-bootstrap.input :as i]
+            [pipfit.web.cljs.login :as l]
             [om-bootstrap.button :as b]
             [om-tools.dom :as d :include-macros :true]
             [cljs-http.client :as http]
@@ -14,42 +15,7 @@
 
 (fw/start {})
 
-(defn sign-in
-  "Sign the user in using ajax."
-  [data owner]
-  (let [username (.-value (om/get-node owner "ufield"))
-        password (.-value (om/get-node owner "pfield"))]
-    (go (let [response (<! (http/post "/login" {:json-params {:username username
-                                                              :password password}}))]
-          (prn (:status response))
-          (prn (:body response))))))
-
-(defn handle-change
-  "Modify state based on changes in username or password field."
-  [e owner k]
-  (om/set-state! owner k (.. e -target -value)))
-
-(defn login-form [data owner]
-  (reify
-    om/IInitState
-    (init-state [_]
-      {:username "" :password ""})
-    om/IRenderState
-    (render-state [this state]
-      (dom/div nil
-       (dom/h2 nil "Login")
-       (dom/div nil
-        (dom/input #js {:type "text"
-                        :ref "ufield"
-                        :onChange #(handle-change % owner :username)
-                        :value (:username state)})
-        (dom/input #js {:type "password"
-                        :ref "pfield"
-                        :onChange #(handle-change % owner :password)
-                        :value (:password state)}) 
-        (dom/button #js {:onClick #(sign-in data owner)} "Submit"))))))
-
 (om/root
-  login-form
+  l/login-form
   {}
   {:target (. js/document (getElementById "app"))})
